c187f6fb19abcd699d55de6d0d13a9fc1450cbe0
Dmitriy <dimkkononov@gmail.com>
2025-08-14 17:03:03 +0300

    add repository

---
+++ internal/repository/postgres/postgres.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/solumD/go-service-template/internal/model"
+	"github.com/solumD/go-service-template/internal/repository"
+)
+
+type entityRepository struct {
+	pool *pgxpool.Pool
+}
+
+func New(pool *pgxpool.Pool) repository.Repository {
+	return &entityRepository{
+		pool: pool,
+	}
+}
+
+func (r *entityRepository) SaveEntity(ctx context.Context, entity *model.Entity) (int64, error) {
+	query := `INSERT INTO entity (name) VALUES ($1) RETURNING id`
+
+	var entityID int64
+	err := r.pool.QueryRow(ctx, query, entity.Name).Scan(&entityID)
+	if err != nil {
+		return 0, fmt.Errorf("[postgres] failed to save entity: %v (name: %s)", err, entity.Name)
+	}
+
+	return entityID, nil
+}
+
+func (r *entityRepository) GetEntity(ctx context.Context, id int64) (*model.Entity, error) {
+	query := `SELECT name FROM entity WHERE id = $1`
+
+	var entityName string
+	err := r.pool.QueryRow(ctx, query, id).Scan(&entityName)
+	if err != nil {
+		return nil, fmt.Errorf("[postgres] failed to get entity: %v (id: %d)", err, id)
+	}
+
+	return &model.Entity{ID: id, Name: entityName}, nil
+}
---
+++ internal/repository/repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/solumD/go-service-template/internal/model"
+)
+
+type Repository interface {
+	SaveEntity(ctx context.Context, entity *model.Entity) (int64, error)
+	GetEntity(ctx context.Context, id int64) (*model.Entity, error)
+}
